057e8ec8413202eed81c9f3145d29f5707ade664
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-03 12:26:45 +0100

    added eslint

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -1,6 +1,6 @@
 export class Header {
   constructor(selector) {
-    this.elt = document.querySelector('header');
+    this.elt = document.querySelector(selector);
   }
 
   setTitle(title) {
--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-'use strict';
-
-import { Menu } from "./lib/Menu";
-import { Header } from "./lib/Header";
-import { DrawingBoard } from "./lib/DrawingBoard";
+import { Menu } from './lib/Menu';
+import { Header } from './lib/Header';
+import { DrawingBoard } from './lib/DrawingBoard';
 
 function main() {
   console.log('starting');
-  new Header().setTitle('The SVG Editor');
+  new Header('header').setTitle('The SVG Editor');
 
   const board = new DrawingBoard('main');
+  console.log('board: ', board);
 
   const addLine = () => console.log('addLine');
   const cleanAll = () => console.log('cleanAll');
