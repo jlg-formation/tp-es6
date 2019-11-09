c069a2881b23f29129a544fc8f8d88930f193a98
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-09 16:23:48 +0100

    added effect on site-id

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -1,9 +1,30 @@
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
 export class Header {
   constructor(selector) {
     this.elt = document.querySelector(selector);
+    this.initEffectOnTitle();
   }
 
   setTitle(title) {
     this.elt.querySelector('.title').innerHTML = title;
+  }
+
+  initEffectOnTitle() {
+    this.elt.querySelector('.title').addEventListener('click', this.onClick.bind(this));
+  }
+
+  async onClick() {
+    const titleElt = this.elt.querySelector('.title');
+    const title = titleElt.innerHTML;
+    console.log('onclick', title);
+
+    for (let i = 0; i <= title.length; i++) {
+      await sleep(200);
+
+      const [before, letter, after] = [title.substr(0, i), title.substr(i, 1), title.substr(i + 1)];
+      const niceTitle = `${before}<span style="color: red">${letter}</span>${after}`;
+      titleElt.innerHTML = niceTitle;
+    }
   }
 }
